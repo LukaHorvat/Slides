8b5a77e5b9319599af9df00b3663ae7631f30c72
Luka Horvat <lukahorvat9@gmail.com>
2015-10-20 22:49:24 +0200

    Removed example project

--- src/Diagrams/Imports.hs
+++
@@ -1,7 +0,0 @@
-module Diagrams.Imports ((<>), module X) where
-
-import Diagrams.Backend.SVG as X
-import Diagrams as X
-import Data.Monoid ((<>))
-import Data.Colour.Names as X
-import Data.Colour.RGBSpace as X
